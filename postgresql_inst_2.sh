97385b1f617b1d209217a6f56a628f59d2557351
cucafresca <50235839+cucafresca@users.noreply.github.com>
2020-04-11 11:02:31 -0300

    Add files via upload

---
+++ postgresql_inst_2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Vamos criar criar a estrutura do Postgresql"
+echo "Aperte enter para continuar"
+read
+
+cd /usr/local/bin
+
+echo "Será solicitado a senha do usuário Postgres digite 123456"
+echo "aperte Enter para continuar"
+
+./initdb -U postgres -W -A md5 --locale=pt_BR.ISO-8859-1 -D /home/cuca/postgresql/10/data/
+
+echo "Estrutura criada com sucesso"
+echo "Aperte ENTER para continuar"
+read
+
+cd /home/cuca/postgres/10/data
+rm -f cucafresca.conf* postgresql.conf* pg_hba.conf*
+
+echo "Agora vamos baixar os arquivos necessarios para configurar o POSTGRESQL"
+echo "Aperte Enter para continuar"
+read
+
+wget https://raw.githubusercontent.com/cucafresca/iniciabanco/master/cucafresca.conf
+
+wget https://raw.githubusercontent.com/cucafresca/iniciabanco/master/postgresql.conf
+
+wget https://raw.githubusercontent.com/cucafresca/iniciabanco/master/pg_hba.conf
+
+echo "Instalação finalizada com sucesso"
+
+
+
+
+
